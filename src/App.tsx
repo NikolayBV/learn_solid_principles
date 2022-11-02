6104c9aee6bdcbc241a546c00c41b64a1401715c
NikolayBV <bnv95o@yandex.ru>
2022-11-02 23:01:47 +0600

    description ISP principle

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,9 @@ import {Link} from "react-router-dom";
 
 //violation of solid principles
 //1. The SRP component is responsible for several actions
-// 2. OCP - if it is necessary to add a new component, the logic will be modified
-// 5. The DIP function of getting an array of users depends on a specific url
+//2. OCP - if it is necessary to add a new component, the logic will be modified
+//4. ISP - the processing of all fields of the user's object that are not currently in use can be considered a violation of this principle
+//5. The DIP function of getting an array of users depends on a specific url
 
 
 function App() {
